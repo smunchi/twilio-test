591f63999740e71c6b7bff3ef4bf0835343d502d
Md. Salahuddin <salah_cse_mbstu@yahoo.com>
2020-06-28 13:37:44 +0600

    update credentials

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
 
 // Your Account SID and Auth Token from twilio.com/console
-$account_sid = 'AC33aa177fd40339e36a82f78d5727d1bf';
-$auth_token = '8015269d9a13dd1d3e1394e2dfd943cb';
+$account_sid = 'XXXXXXXXXXXXXXX';
+$auth_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
 
 // A Twilio number you own with SMS capabilities
-$twilio_number = "+12058929837";
+$twilio_number = "+120589XXXXX";
 
 $client = new Client($account_sid, $auth_token);
 $client->messages->create(
     // Where to send a text message (your cell phone?)
-    '+8801723523983',
+    '+8801723XXXXXX',
     array(
         'from' => $twilio_number,
         'body' => 'I sent this message in under 10 minutes!'
